5cceda50cc4fe9e02b38943a4e8248df5923b8c7
VictorChukwudi <victorukay@gmail.com>
2024-02-06 22:52:42 +0100

    implements repository design pattern

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -1,66 +1,38 @@
 using GameStore.Api.Entities;
+using GameStore.Api.Repositories;
 namespace GameStore.Api.Endpoints;
 
 public static class GamesEndpoints
 {
     const string getGameEndpointName = "GetGame";
 
-    static List<Game> games = new(){
-            new Game(){
-                Id = 1,
-                Name= "Street Fighter",
-                Genre= "Action",
-                Price= 19.99M,
-                ReleaseDate=new DateTime(1991, 2, 1),
-                ImageUrl="https://placehold.co/100"
-            },
-            new Game(){
-                Id = 2,
-                Name= "Final Fantasy XIV",
-                Genre= "Role Playing",
-                Price= 59.99M,
-                ReleaseDate=new DateTime(2010, 9, 10),
-                ImageUrl="https://placehold.co/100"
-            },
-            new Game(){
-                Id = 3,
-                Name= "FIFA 23",
-                Genre= "Sports",
-                Price= 69.99M,
-                ReleaseDate=new DateTime(2022, 9, 27),
-                ImageUrl="https://placehold.co/100"
-            },
-        };
+    
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
+        InMemRepository repository=new();
         //route group - similar to route groups in expressjs
         var group = routes.MapGroup("/games")
                         .WithParameterValidation();
 
-        group.MapGet("/", () => games);
+        group.MapGet("/", () => repository.GetAll());
 
         group.MapGet("/{id}", (int id) =>
         {
-            Game? game = games.Find(game => game.Id == id);
-            if (game is null)
-            {
-                return Results.NotFound();
-            }
-            return Results.Ok(game);
+            Game? game = repository.Get(id); 
+            return game is not null ? Results.Ok(game): Results.NotFound();
         }).WithName(getGameEndpointName);
 
         group.MapPost("/", (Game game) =>
         {
-            game.Id = games.Max(game => game.Id) + 1;
-            games.Add(game);
-            Console.WriteLine(games.Count);
+            repository.Create(game);
+            // Console.WriteLine(games.Count);
 
             return Results.CreatedAtRoute(getGameEndpointName, new { id = game.Id }, game);
         });
 
         group.MapPut("/{id}", (int id, Game updatedGame) =>
         {
-            Game? existingGame = games.Find(game => game.Id == id);
+            Game? existingGame =repository.Get(id);
             if (existingGame is null)
             {
                 return Results.NotFound();
@@ -72,17 +44,18 @@ public static class GamesEndpoints
             existingGame.Price = updatedGame.Price;
             existingGame.ImageUrl = updatedGame.ImageUrl;
 
+            repository.Update(existingGame);
             return Results.NoContent();
         });
 
         group.MapDelete("/{id}", (int id) =>
         {
-            Game? game = games.Find(game => game.Id == id);
-            if (game is not null)
-            {
-                games.Remove(game);
-            }
-            return Results.NoContent();
+            Game? game =repository.Get(id);
+           if(game is not null){
+            repository.Delete(id);
+           }
+
+           return Results.NoContent();
         });
 
         return group;
---
+++ GameStore.Api/Repositories/InMemRepository.cs
@@ -0,0 +1,55 @@
+using GameStore.Api.Entities;
+namespace GameStore.Api.Repositories;
+
+public class InMemRepository
+{
+    private readonly List<Game> games = new(){
+            new Game(){
+                Id = 1,
+                Name= "Street Fighter",
+                Genre= "Action",
+                Price= 19.99M,
+                ReleaseDate=new DateTime(1991, 2, 1),
+                ImageUrl="https://placehold.co/100"
+            },
+            new Game(){
+                Id = 2,
+                Name= "Final Fantasy XIV",
+                Genre= "Role Playing",
+                Price= 59.99M,
+                ReleaseDate=new DateTime(2010, 9, 10),
+                ImageUrl="https://placehold.co/100"
+            },
+            new Game(){
+                Id = 3,
+                Name= "FIFA 23",
+                Genre= "Sports",
+                Price= 69.99M,
+                ReleaseDate=new DateTime(2022, 9, 27),
+                ImageUrl="https://placehold.co/100"
+            },
+        };
+
+        public IEnumerable<Game> GetAll(){
+            return games;
+        }
+
+        public Game? Get(int id){
+           return games.Find(game => game.Id == id);
+        }
+
+        public void Create(Game game){
+            game.Id = games.Max(game => game.Id) + 1;
+             games.Add(game);
+        }
+
+        public void Update(Game updatedGame){
+            var index= games.FindIndex(game=> game.Id == updatedGame.Id);
+           games[index]= updatedGame;
+        }
+
+        public void Delete(int id){
+            var index = games.FindIndex(game=> game.Id == id);
+            games.RemoveAt(index);
+        }
+}
